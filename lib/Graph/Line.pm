243a8e14d405ec975a361f5a8ecd93317b1e43c0
Andrius Merkys <andrius.merkys@gmail.com>
2020-12-29 12:29:26 +0200

    Fixing linear graph generation.

--- lib/Graph/Line.pm
+++ lib/Graph/Line.pm
@@ -3,35 +3,49 @@ package Graph::Line;
 use strict;
 use warnings;
 
+use parent 'Graph::Undirected';
+
 use Graph::Undirected;
 
 # VERSION
 
 sub new
 {
-    my( $graph, $options ) = @_;
+    my( $class, $graph, $options ) = @_;
 
     $options = {} unless $options;
 
-    my @edges = $graph->edges;
+    my @edges = map { { orig => $_,
+                        attr => $graph->get_edge_attributes( @$_ ) } }
+                    $graph->edges;
     my $adjacency = {};
     for my $edge (@edges) {
-        push @{$adjacency->{$edge->[0]}}, $edge;
-        push @{$adjacency->{$edge->[1]}}, $edge;
+        push @{$adjacency->{$edge->{orig}[0]}}, $edge;
+        push @{$adjacency->{$edge->{orig}[1]}}, $edge;
     }
 
+    # Create the line graph
     my $line_graph = Graph::Undirected->new;
     $line_graph->add_vertices( @edges );
     for my $vertex (sort keys %$adjacency) {
-        for my $i (0..$#${$adjacency->{$vertex}}-1) {
-            for my $j ($i+1..$#${$adjacency->{$vertex}}) {
+        for my $i (0..$#{$adjacency->{$vertex}}-1) {
+            for my $j ($i+1..$#{$adjacency->{$vertex}}) {
                 $line_graph->add_edge( $adjacency->{$vertex}[$i],
                                        $adjacency->{$vertex}[$j] );
             }
         }
     }
 
-    return $line_graph;
+    # Leave only old edge attributes in new vertices
+    for my $vertex ($line_graph->vertices) {
+        my $attr = $vertex->{attr};
+        delete $vertex->{attr};
+        delete $vertex->{orig};
+        next if !defined $attr;
+        %$vertex = %$attr;
+    }
+
+    return bless $line_graph, $class;
 }
 
 1;
