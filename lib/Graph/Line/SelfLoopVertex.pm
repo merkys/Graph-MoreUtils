7fe5919a25146597dae26c495c545a18ebc34546
Andrius Merkys <andrius.merkys@gmail.com>
2021-02-04 19:02:37 +0200

    Adding POD for Graph::Line::SelfLoopVertex.

--- lib/Graph/Line/SelfLoopVertex.pm
+++ lib/Graph/Line/SelfLoopVertex.pm
@@ -1,5 +1,11 @@
 package Graph::Line::SelfLoopVertex;
 
+use strict;
+use warnings;
+
+# ABSTRACT: Marker for artificial self-loops
+# VERSION
+
 sub new
 {
     my( $class ) = @_;
@@ -7,3 +13,28 @@ sub new
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Graph::Line::SelfLoopVertex - marker for artificial self-loops
+
+=head1 DESCRIPTION
+
+With C<loop_end_vertices> option L<Graph::Line|Graph::Line> adds
+self-loops on pendant vertices thus preserving them from getting "lost".
+Instances of Graph::Line::SelfLoopVertex are used to represent the
+artificial self-loops in the resulting line graphs.
+
+=head1 SEE ALSO
+
+perl(1)
+
+=head1 AUTHORS
+
+Andrius Merkys, E<lt>merkys@cpan.orgE<gt>
+
+=cut
