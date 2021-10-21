d1ddfb3842939f5bb0c474bcd6377d0db255cd22
Andrius Merkys <andrius.merkys@gmail.com>
2021-10-21 16:38:29 +0300

    Initial implementation.

---
+++ lib/Graph/Smoothed.pm
@@ -0,0 +1,69 @@
+package Graph::Smoothed;
+
+use strict;
+use warnings;
+
+use Graph::Smoothed::Intermediate;
+use Graph::Undirected;
+use Scalar::Util qw( blessed );
+
+use parent Graph::Undirected::;
+
+# ABSTRACT: Generate smoothed graphs
+# VERSION
+
+sub new
+{
+    my( $class, $graph ) = @_;
+
+    if( !blessed $graph || !$graph->isa( Graph::Undirected:: ) ) {
+        die 'only Graph::Undirected and its derivatives accepted' . "\n";
+    }
+
+    my $graph_now = $graph->copy;
+    for ($graph_now->vertices) {
+        next unless $graph_now->degree( $_ ) != 2;
+        my( $a, $b ) = sort $graph_now->neighbours( $_ );
+
+        # do not reduce cycles of three vertices:
+        next if $graph_now->has_edge( $a, $b );
+
+        my $intermediate;
+        if( $graph_now->has_edge_attribute( $a, $_, 'intermediate' ) &&
+            $graph_now->has_edge_attribute( $b, $_, 'intermediate' ) ) {
+            if(      $_ lt $a ) {
+                $intermediate = Graph::Smoothed::Intermediate->new(
+                    $graph_now->get_edge_attribute( $a, $_, 'intermediate' )->reverse,
+                    $_,
+                    $graph_now->get_edge_attribute( $b, $_, 'intermediate' ) );
+            } elsif( $_ gt $b ) {
+                $intermediate = Graph::Smoothed::Intermediate->new(
+                    $graph_now->get_edge_attribute( $a, $_, 'intermediate' ),
+                    $_,
+                    $graph_now->get_edge_attribute( $b, $_, 'intermediate' )->reverse );
+            } else {
+                $intermediate = Graph::Smoothed::Intermediate->new(
+                    $graph_now->get_edge_attribute( $a, $_, 'intermediate' ),
+                    $_,
+                    $graph_now->get_edge_attribute( $b, $_, 'intermediate' ) );
+            }
+        } elsif( $graph_now->has_edge_attribute( $a, $_, 'intermediate' ) ) {
+            $intermediate = $graph_now->get_edge_attribute( $a, $_, 'intermediate' );
+            $intermediate->reverse if $a gt $_; # getting natural order
+            push @$intermediate, $_;
+        } elsif( $graph_now->has_edge_attribute( $b, $_, 'intermediate' ) ) {
+            $intermediate = $graph_now->get_edge_attribute( $b, $_, 'intermediate' );
+            $intermediate->reverse if $_ gt $b; # getting natural order
+            unshift @$intermediate, $_;
+        } else {
+            $intermediate = Graph::Smoothed::Intermediate->new( $_ );
+        }
+
+        $graph_now->delete_vertex( $_ );
+        $graph_now->set_edge_attribute( $a, $b, $intermediate );
+    }
+
+    return bless $graph_now, $class;
+}
+
+1;
---
+++ lib/Graph/Smoothed/Intermediate.pm
@@ -0,0 +1,24 @@
+package Graph::Smoothed::Intermediate;
+
+use strict;
+use warnings;
+
+use Scalar::Util qw( blessed );
+
+# ABSTRACT: Generate smoothed graphs
+# VERSION
+
+sub new {
+    my $class = shift;
+    my $self = [ map { blessed $_ &&
+                       $_->isa( Graph::Smoothed::Intermediate:: ) ? @$_ : $_ } @_ ];
+    return bless $self, $class;
+}
+
+sub reverse {
+    my( $self ) = @_;
+    @$self = reverse @$self;
+    return $self;
+}
+
+1;
