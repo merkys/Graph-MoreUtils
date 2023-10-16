b1034e669788225de5f5e5e57ab03928e2b9ae6d
Andrius Merkys <andrius.merkys@gmail.com>
2023-10-16 09:39:26 +0300

    Moving README.md to POD of Graph::MoreUtils::SSSR.

--- lib/Graph/MoreUtils/SSSR.pm
+++ lib/Graph/MoreUtils/SSSR.pm
@@ -6,6 +6,23 @@ use warnings;
 # ABSTRACT: Find Smallest Set of Smallest Rings in graphs
 # VERSION
 
+=head1 NAME
+
+Graph::MoreUtils::SSSR - find Smallest Set of Smallest Rings in graphs
+
+=head1 DESCRIPTION
+
+This is an implementation of Smallest Set of Smallest Rings (SSSR) finding algorithm based on L<Graph>.
+Thus it should work with any L<Graph::Undirected> object.
+The code is largely taken from the C<cod-tools> package (L<https://wiki.crystallography.net/cod-tools/>).
+
+The algorithm returns a superset of minimum cycle basis of a graph in order to produce deterministic results.
+As a result it does not succumb to the counterexample of oxabicyclo[2.2.2]octane (L<https://depth-first.com/articles/2020/08/31/a-smallest-set-of-smallest-rings/>, section "SSSR and Uniqueness").
+The algorithm has means to control the maximum size of rings included in the SSSR to reduce its complexity.
+The default value of C<undef> stands for no limit.
+
+=cut
+
 sub get_SSSR
 {
     my( $graph, $max_depth ) = @_;
