a97e88c7f3807f54eddce7470cc3f09968132670
Andrius Merkys <andrius.merkys@gmail.com>
2025-01-08 11:55:27 +0200

    Add graph from https://pallini.di.uniroma1.it/SearchTree.html. Fix individualise_dfs(). Drop dead code from nauty-graph-test.pl.

--- nauty-graph-test.pl
+++ nauty-graph-test.pl
@@ -34,37 +34,6 @@ sub color_by_orbits
     return %colors;
 }
 
-sub colors_to_orbits
-{
-    my %colors = @_;
-    my @orbits;
-    for (sort keys %colors) {
-        push @{$orbits[$colors{$_}]}, $_;
-    }
-    return @orbits;
-}
-
-sub individualise_until_discrete
-{
-    my $graph = shift;
-    my %colors = @_;
-
-    return %colors if uniq( values %colors ) == $graph->vertices;
-
-    my $vertex;
-    for my $color (sort uniq values %colors) {
-        my @orbit = grep { $colors{$_} == $color } sort keys %colors;
-        next unless @orbit > 1;
-
-        $vertex = shift @orbit;
-    }
-
-    %colors = individualise( %colors, $vertex );
-    my @orbits = orbits( $graph, sub { $colors{$_[0]} } );
-
-    return individualise_until_discrete( $graph, color_by_orbits( @orbits ) );
-}
-
 sub individualise_dfs
 {
     my $graph = shift;
@@ -80,24 +49,39 @@ sub individualise_dfs
 
     my @partitions;
     for (@orbit) {
-        push @partitions, individualise_dfs( $graph, individualise( %colors, $_ ) );
+        my %colors = individualise( %colors, $_ );
+        my @orbits = orbits( $graph, sub { $colors{$_[0]} } );
+        push @partitions, individualise_dfs( $graph, color_by_orbits( @orbits ) );
     }
     return @partitions;
 }
 
 my $g = Graph::Undirected->new;
-$g->add_cycle( '01', '04', '09', '05', '02', '06', '10', '03' );
-$g->add_path( '03', '07', '05' );
-$g->add_path( '04', '08', '06' );
-$g->add_edge( '07', '09' );
-$g->add_edge( '08', '10' );
+#~ $g->add_cycle( '01', '04', '09', '05', '02', '06', '10', '03' );
+#~ $g->add_path( '03', '07', '05' );
+#~ $g->add_path( '04', '08', '06' );
+#~ $g->add_edge( '07', '09' );
+#~ $g->add_edge( '08', '10' );
+$g->add_cycle( 0..4 );
+$g->add_cycle( 5, 7, 9, 6, 8 );
+for (0..4) {
+    $g->add_edge( $_, 5 + $_ );
+}
 
 my %colors;
 for ($g->vertices) {
-    $colors{$_} = 0 + ($_ > 2);
+    $colors{$_} = 0; # + ($_ > 2);
 }
 
 my @orbits = orbits( $g, sub { $colors{$_[0]} } );
 %colors = color_by_orbits( @orbits );
 
-print STDERR scalar individualise_dfs( $g, %colors );
+my @partitions = individualise_dfs( $g, %colors );
+my %freq;
+for my $partition (@partitions) {
+    for my $vertex (keys %$partition) {
+        $freq{$vertex}->{$partition->{$vertex}}++;
+    }
+}
+print scalar @partitions, "\n";
+# print STDERR Dumper \%freq;
