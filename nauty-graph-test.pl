5ffac1687c9f174d2b42bfc1df70d3220e67fae4
Andrius Merkys <andrius.merkys@gmail.com>
2025-01-10 15:56:37 +0200

    More examples.

--- nauty-graph-test.pl
+++ nauty-graph-test.pl
@@ -54,7 +54,7 @@ sub refine
         print "new cells to check: @cells_to_check";
     }
 
-    print "@cells";
+    return @cells;
 }
 
 my $g = Graph::Undirected->new;
@@ -65,7 +65,9 @@ $g->add_path( 3, 4, 5 );
 
 # print Dumper [ orbits( $g, sub { '' } ) ];
 
-refine( $g, '5', set( '1', '3', '5', '7' ), set( '0', '2', '6', '8' ), set( '4' ) );
+#~ my @cells = refine( $g, '5', set( '1', '3', '5', '7' ), set( '0', '2', '6', '8' ), set( '4' ) );
+#~ refine( $g, '7', @cells );
+#~ refine( $g, '1', @cells );
 
 $g = Graph::Undirected->new;
 $g->add_cycle( 1, 4, 9, 5, 2, 6, 10, 3 );
@@ -74,4 +76,17 @@ $g->add_path( 4, 8, 6 );
 $g->add_edge( 7, 9 );
 $g->add_edge( 8, 10 );
 
-# refine( $g, '3', set( '1', '2' ), set( '3', '4', '5', '6' ), set( '7', '8', '9', '10' ) );
+#~ my @cells = refine( $g, '3', set( '1', '2' ), set( '3', '4', '5', '6' ), set( '7', '8', '9', '10' ) );
+#~ refine( $g, '5', @cells );
+
+$g = Graph::Undirected->new;
+$g->add_cycle( 0..4 );
+$g->add_cycle( 5, 7, 9, 6, 8 );
+for (0..4) {
+    $g->add_edge( $_, 5 + $_ );
+}
+
+my @cells = refine( $g, '0', set( '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ) );
+@cells = refine( $g, '1', @cells );
+refine( $g, '3', @cells );
+refine( $g, '8', @cells );
