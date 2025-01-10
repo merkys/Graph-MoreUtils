5ebdf65f5dd451546eb317b4e267e64609cd1126
Andrius Merkys <andrius.merkys@gmail.com>
2025-01-10 15:47:42 +0200

    It seems that the refinement code is working.

--- nauty-graph-test.pl
+++ nauty-graph-test.pl
@@ -26,26 +26,35 @@ sub refine
     @cells = ( @cells[0..$cell-1], set( $vertex ), @cells[$cell..$#cells] );
     print "cells after  individualisation: @cells";
 
-    my @cells_now;
-    for my  $affected_cell (@cells) {
-        if( $affected_cell == $cells[$cell] || $affected_cell->size == 1 ) {
-            push @cells_now, $affected_cell;
-            next;
-        }
+    my @cells_to_check = ( $cells[$cell] );
+    while( @cells_to_check ) {
+        my $cell = shift @cells_to_check; print "check $cell";
 
-        my @neighbours_per_vertex;
-        for my $vertex (@$affected_cell) {
-            my $neighbours = (set( $graph->neighbours( $vertex ) ) * $cells[$cell])->size;
-            print $vertex, ' ', $neighbours;
-            $neighbours_per_vertex[$neighbours] = [] unless $neighbours_per_vertex[$neighbours];
-            push @{$neighbours_per_vertex[$neighbours]}, $vertex;
-        }
+        my @cells_now;
+        for my  $affected_cell (@cells) {
+            if( $affected_cell == $cell || $affected_cell->size == 1 ) {
+                push @cells_now, $affected_cell;
+                next;
+            }
 
-        my @new_cells = map { set( @$_ ) } reverse grep { $_ } @neighbours_per_vertex;
-        push @cells_now, @new_cells;
+            my @neighbours_per_vertex;
+            for my $vertex (@$affected_cell) {
+                my $neighbours = (set( $graph->neighbours( $vertex ) ) * $cell)->size;
+                print $vertex, ' ', $neighbours;
+                $neighbours_per_vertex[$neighbours] = [] unless $neighbours_per_vertex[$neighbours];
+                push @{$neighbours_per_vertex[$neighbours]}, $vertex;
+            }
+
+            my @new_cells = map { set( @$_ ) } reverse grep { $_ } @neighbours_per_vertex;
+            push @cells_now, @new_cells;
+            push @cells_to_check, @new_cells[1..$#new_cells] if @new_cells > 1;
+        }
+        @cells = @cells_now;
+        print "after checking: @cells";
+        print "new cells to check: @cells_to_check";
     }
 
-    print "@cells_now";
+    print "@cells";
 }
 
 my $g = Graph::Undirected->new;
@@ -56,7 +65,7 @@ $g->add_path( 3, 4, 5 );
 
 # print Dumper [ orbits( $g, sub { '' } ) ];
 
-# refine( $g, '5', set( '1', '3', '5', '7' ), set( '0', '2', '6', '8' ), set( '4' ) );
+refine( $g, '5', set( '1', '3', '5', '7' ), set( '0', '2', '6', '8' ), set( '4' ) );
 
 $g = Graph::Undirected->new;
 $g->add_cycle( 1, 4, 9, 5, 2, 6, 10, 3 );
@@ -65,4 +74,4 @@ $g->add_path( 4, 8, 6 );
 $g->add_edge( 7, 9 );
 $g->add_edge( 8, 10 );
 
-refine( $g, '3', set( '1', '2' ), set( '3', '4', '5', '6' ), set( '7', '8', '9', '10' ) );
+# refine( $g, '3', set( '1', '2' ), set( '3', '4', '5', '6' ), set( '7', '8', '9', '10' ) );
