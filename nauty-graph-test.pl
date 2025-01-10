8e90d0757f81a941a7e1c93bc3f91036086a2305
Andrius Merkys <andrius.merkys@gmail.com>
2025-01-10 14:40:13 +0200

    Minor code reordering.

--- nauty-graph-test.pl
+++ nauty-graph-test.pl
@@ -9,12 +9,6 @@ use Graph::Undirected;
 use List::Util qw( any first );
 use Set::Object qw( set );
 
-my $g = Graph::Undirected->new;
-$g->add_cycle( 0, 1, 2, 5, 8, 7, 6, 3 );
-$g->add_cycle( 1, 3, 7, 5 );
-$g->add_path( 1, 4, 7 );
-$g->add_path( 3, 4, 5 );
-
 $Graph::MoreUtils::Isomorphism::debug = 1;
 
 local $\ = "\n";
@@ -54,6 +48,12 @@ sub refine
 
     print "@cells_now";
 }
+
+my $g = Graph::Undirected->new;
+$g->add_cycle( 0, 1, 2, 5, 8, 7, 6, 3 );
+$g->add_cycle( 1, 3, 7, 5 );
+$g->add_path( 1, 4, 7 );
+$g->add_path( 3, 4, 5 );
 
 # print Dumper [ orbits( $g, sub { '' } ) ];
 
